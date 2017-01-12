8440b0214b438a28828f6823c70c53acdb85f1b8
Jason Brown <jasontbrown99@gmail.com>
2017-01-12 15:46:00 -0800

    added base js rules. div id match is now inline with celeb ids

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("form#about-you").submit(function(event){
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+    var hobbies = $("select#hobbies").val();
+    var food = $("select#food").val());
+
+    if (age === <= 26 && gender === male) {
+      var = match $("").val()
+    }
+
+    event.preventDefault();
+  });
+});
