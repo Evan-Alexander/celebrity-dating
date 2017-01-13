3c1a7a820b3e44e10a5b01fb8b58cb6ebfd80036
Jason Brown <jasontbrown99@gmail.com>
2017-01-12 16:29:12 -0800

    think I have my rules sorted for male choices. seeing if this works

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,40 @@ $(document).ready(function() {
     var hobbies = $("select#hobbies").val();
     var food = $("select#food").val());
 
-    if (gender === 'male' && age <= 30) {
-      var = kate $("").val()
+
+    if (gender === 'male' && age <= 30 ) {
+      $("#upton").show();
+      $("#oprah").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
+    } else if (gender === 'male' || age <= 30 || 'reading' || 'italian') {
+      $("#upton").show();
+      $("#oprah").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
+    } else if (gender === 'male' &&  age <=31 ) {
+      $("#oprah").show();
+      $("#upton").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
+    } else if (gender === 'male' ||  age <=31 || 'italian' || 'reading') {
+      $("#oprah").show();
+      $("#upton").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
+    }
+
+
+
+
+
+
+
+
     }
 
     $("#celeb").empty().append();
-    $("")
+    $("#match").show();
 
     event.preventDefault();
   });
