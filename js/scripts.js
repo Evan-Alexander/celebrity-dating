e27a0aa04db6b9d88b0de56b774d8f220f1191ec
Jason Brown <jasontbrown99@gmail.com>
2017-01-12 17:57:11 -0800

    got the append portion of the site to work by creating a var = celeb and just throwing a string in there

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,25 @@ $(document).ready(function() {
 
 
     if (gender === 'male' && age <= 30 ) {
+      var celeb = ("Kate Upton");
       $("#upton").show();
       $("#oprah").hide();
       $("#keaton").hide();
       $("#burr").hide();
     } else if (gender === 'male' && age < 30 && hobbies === 'reading' && food === 'italian') {
+      var celeb = ("Oprah Winfrey");
       $("#oprah").show();
       $("#upton").hide();
       $("#keaton").hide();
       $("#burr").hide();
     } else if (gender === 'male' && age > 30 ) {
+      var celeb = ("Oprah Winfrey");
       $("#oprah").show();
       $("#upton").hide();
       $("#keaton").hide();
       $("#burr").hide();
     } else if (gender === 'male' && age > 30 && hobbies === 'outdoor' && food === 'thai') {
+      var celeb = ("Kate Upton");
       $("#upton").show();
       $("#oprah").hide();
       $("#keaton").hide();
@@ -29,28 +33,32 @@ $(document).ready(function() {
     }
 
     if (gender === 'female' && age <= 30 ) {
+      var celeb = ("Micheal Keaton");
       $("#keaton").show();
       $("#oprah").hide();
       $("#upton").hide();
       $("#burr").hide();
     } else if (gender === 'female' && age < 30 && hobbies === 'reading' && food === 'italian') {
+      var celeb = ("Bill Burr");
       $("#burr").show();
       $("#upton").hide();
       $("#keaton").hide();
       $("#oprah").hide();
     } else if (gender === 'female' && age > 30 ) {
+      var celeb = ("Bill Burr");
       $("#burr").show();
       $("#upton").hide();
       $("#keaton").hide();
       $("#oprah").hide();
     } else if (gender === 'female' && age > 30 && hobbies === 'outdoor' && food === 'thai') {
+      var celeb = ("Micheal Keaton");
       $("#keaton").show();
       $("#oprah").hide();
       $("#keaton").hide();
       $("#burr").hide();
     }
 
-    $("#celeb").empty().append();
+    $("#celeb").empty().append(celeb);
     $("#match").show();
 
     event.preventDefault();
