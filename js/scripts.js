a69baa0d5f652b06c7029e3812cc5288101cc99c
Jason Brown <jasontbrown99@gmail.com>
2017-01-12 17:51:29 -0800

    got male and female choices to work.  not entirely happy with the logic, but it works

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
     var hobbies = $("select#hobbies").val();
-    var food = $("select#food").val());
+    var food = $("select#food").val();
 
 
     if (gender === 'male' && age <= 30 ) {
@@ -11,30 +11,43 @@ $(document).ready(function() {
       $("#oprah").hide();
       $("#keaton").hide();
       $("#burr").hide();
-    } else if (gender === 'male' || age <= 30 || 'reading' || 'italian') {
+    } else if (gender === 'male' && age < 30 && hobbies === 'reading' && food === 'italian') {
+      $("#oprah").show();
+      $("#upton").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
+    } else if (gender === 'male' && age > 30 ) {
+      $("#oprah").show();
+      $("#upton").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
+    } else if (gender === 'male' && age > 30 && hobbies === 'outdoor' && food === 'thai') {
       $("#upton").show();
       $("#oprah").hide();
       $("#keaton").hide();
       $("#burr").hide();
-    } else if (gender === 'male' &&  age <=31 ) {
-      $("#oprah").show();
-      $("#upton").hide();
-      $("#keaton").hide();
-      $("#burr").hide();
-    } else if (gender === 'male' ||  age <=31 || 'italian' || 'reading') {
-      $("#oprah").show();
-      $("#upton").hide();
-      $("#keaton").hide();
-      $("#burr").hide();
     }
 
-
-
-
-
-
-
-
+    if (gender === 'female' && age <= 30 ) {
+      $("#keaton").show();
+      $("#oprah").hide();
+      $("#upton").hide();
+      $("#burr").hide();
+    } else if (gender === 'female' && age < 30 && hobbies === 'reading' && food === 'italian') {
+      $("#burr").show();
+      $("#upton").hide();
+      $("#keaton").hide();
+      $("#oprah").hide();
+    } else if (gender === 'female' && age > 30 ) {
+      $("#burr").show();
+      $("#upton").hide();
+      $("#keaton").hide();
+      $("#oprah").hide();
+    } else if (gender === 'female' && age > 30 && hobbies === 'outdoor' && food === 'thai') {
+      $("#keaton").show();
+      $("#oprah").hide();
+      $("#keaton").hide();
+      $("#burr").hide();
     }
 
     $("#celeb").empty().append();
